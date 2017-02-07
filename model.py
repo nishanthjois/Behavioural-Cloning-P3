501baab24151180409655f7eb5d66af4850921d2
nishanthjois <nishanth.jois@gmail.com>
2017-02-08 00:39:38 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
+# Udacity SDND's project 3: Use Deep Learning to Clone Driving Behaviour
+# Author: Nishanth Jois
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
